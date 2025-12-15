00c07224a7d0403bee8e6f61bf0102c2f0d749a2
Julian_Glass-Pilon <julian@astucemedia.com>
2025-12-15 13:26:14 -0500

    Created page buttons structure and transition logic. Added animation transition logic to customToggle for normal, highlight, pressed and selected

--- Assets/SampleContent/Scripts/CustomToggle.cs
+++ Assets/SampleContent/Scripts/CustomToggle.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Button))]
-public class CustomToggle : MonoBehaviour
+public class CustomToggle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField]
+    [Header("Button Transition Based Elements")]
     private GameObject m_activeGraphic;
 
     [SerializeField]
+    [Header("Animation Transition Based Elements")]
+    private Animator m_transitionAnim;
+
+    [SerializeField]
+    private string m_normalTrigger = "Normal", m_highlightTrigger = "Highlighted", m_pressedTrigger = "Pressed", m_selectedTrigger = "Selected", m_disabledTrigger = "Disabled";
+
+    [SerializeField]
+    [Header("Toggle Elements")]
     private CustomToggleGroup m_toggleGroup;
 
     [SerializeField]
@@ -27,9 +37,47 @@ public class CustomToggle : MonoBehaviour
             m_toggleGroup.UnsubscribeToggle(this);
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (m_transitionAnim == null || m_isOn)
+            return;
+
+        m_transitionAnim.SetTrigger(m_highlightTrigger);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (m_transitionAnim == null || m_isOn)
+            return;
+
+        m_transitionAnim.SetTrigger(m_normalTrigger);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (m_transitionAnim == null || (m_isOn && !m_toggleGroup.CanTurnOffToggles))
+            return;
+
+        if (m_toggleGroup != null)
+            m_toggleGroup.DispatchToggleEventToGroup(true, this);
+
+        m_transitionAnim.SetTrigger(m_pressedTrigger);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Toggle();
+
+        if (m_transitionAnim == null || (m_isOn && !m_toggleGroup.CanTurnOffToggles))
+            return;
+
+        string stateTrigger = m_isOn ? m_selectedTrigger : m_normalTrigger;
+        m_transitionAnim.SetTrigger(stateTrigger);
+    }
+
     public void Toggle()
     {
-        if (m_isOn)
+        if (m_isOn && m_toggleGroup.CanTurnOffToggles)
             TurnOff();
         else
             TurnOn();
@@ -57,6 +105,14 @@ public class CustomToggle : MonoBehaviour
     public void SetGraphic(bool isOn)
     {
         m_isOn = isOn;
-        m_activeGraphic.SetActive(isOn);
+
+        if(m_activeGraphic != null)
+            m_activeGraphic.SetActive(isOn);
+
+        if (m_transitionAnim == null)
+            return;
+
+        string stateTrigger = isOn ? m_selectedTrigger : m_normalTrigger;
+        m_transitionAnim.SetTrigger(stateTrigger);
     }
 }
