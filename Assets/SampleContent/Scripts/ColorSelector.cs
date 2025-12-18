9a1531babe4047ba96188eb51c5fdb741f568fbe
Julian_Glass-Pilon <julian@astucemedia.com>
2025-12-18 10:34:50 -0500

    Changed the contrast ratio color selection to use the same UI (cycle selector) as all the other pages for a more consistent UX across pages

--- Assets/SampleContent/Scripts/ColorSelector.cs
+++ Assets/SampleContent/Scripts/ColorSelector.cs
@@ -1,18 +1,53 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 [RequireComponent(typeof(MaskableGraphic))]
 public class ColorSelector : ContrastCalculateInvoker
 {
-    private Image m_targetImage;
+    [SerializeField]
+    [Tooltip("Specify the path to the desired style sheet, relative to the resources folder.")]
+    private string m_pathToStyleSheet = "StyleSheet";
 
-    public void SetColorFromSwab(Image source)
+    private MaskableGraphic m_targetImage;
+    private StyleSheet m_styleSheet;
+    private int m_colorIndex = 0;
+    private Color[] m_contentColors = new Color[0];
+
+    private void Awake()
     {
-        if(m_targetImage == null)
-            m_targetImage = GetComponent<Image>();
+        m_styleSheet = Resources.Load<StyleSheet>(m_pathToStyleSheet);
 
-        m_targetImage.color = source.color;
+        if (m_styleSheet == null)
+        {
+            Debug.LogError($"Failed to load style sheet from Resoures/{m_pathToStyleSheet}");
+            return;
+        }
+
+        m_contentColors = m_styleSheet.ContentColors.Select(x => x.Value).ToArray();
+    }
+
+    public void CycleColor(int increment)
+    {
+        int max = m_contentColors.Length - 1;
+        m_colorIndex += increment;
+
+        if (m_colorIndex < 0)
+            m_colorIndex = max;
+        else if (m_colorIndex > max)
+            m_colorIndex = 0;
+
+        UpdateColorFromIndex(m_colorIndex);
+    }
+
+    private void UpdateColorFromIndex(int colorIndex)
+    {
+        if (m_targetImage == null)
+            m_targetImage = GetComponent<MaskableGraphic>();
+
+        m_targetImage.color = m_contentColors[colorIndex];
         RecaculateContrastRatios();
     }
 }
